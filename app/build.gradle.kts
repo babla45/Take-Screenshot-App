00923130bb50dec362e6c476605e0b869175620d
Md Babla Islam <bilasislam5643@gmail.com>
2026-05-05 23:19:53 +0600

    version 2 and proguard added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,15 +14,16 @@ android {
         applicationId = "com.example.takess"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+	    isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
